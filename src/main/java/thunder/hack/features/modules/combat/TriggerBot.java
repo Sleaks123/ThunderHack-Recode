7360c4fbd10cd1e2eb0d821cc268fec6ca9995e4
idk <115431008+Sleaks123@users.noreply.github.com>
2024-10-04 17:24:17 -0700

    Update TriggerBot.java

--- src/main/java/thunder/hack/features/modules/combat/TriggerBot.java
+++ src/main/java/thunder/hack/features/modules/combat/TriggerBot.java
@@ -12,87 +12,84 @@ import thunder.hack.events.impl.PlayerUpdateEvent;
 import thunder.hack.features.modules.Module;
 import thunder.hack.setting.Setting;
 import thunder.hack.setting.impl.BooleanSettingGroup;
-import java.util.Random;//compilepls
+import java.util.Random;
 
 public final class TriggerBot extends Module {
-    public final Setting<Float> attackRange = new Setting<>("Range", 3.0F, 1.0F, 7.0F);
+    public final Setting<Float> attackRange = new Setting<>("Range", 3f, 1f, 7.0f);
     public final Setting<BooleanSettingGroup> smartCrit = new Setting<>("SmartCrit", new BooleanSettingGroup(true));
-    public final Setting<Boolean> onlySpace;
-    public final Setting<Boolean> autoJump;
-    public final Setting<Boolean> ignoreWalls;
-    public final Setting<Boolean> pauseEating;
-    public final Setting<Boolean> requireWeapon;
-    
+    public final Setting<Boolean> onlySpace = new Setting<>("OnlyCrit", false).addToGroup(smartCrit);
+    public final Setting<Boolean> autoJump = new Setting<>("AutoJump", false).addToGroup(smartCrit);
+    public final Setting<Boolean> ignoreWalls = new Setting<>("IgnoreWalls", false);
+    public final Setting<Boolean> pauseEating = new Setting<>("PauseWhileEating", false);
+    public final Setting<Integer> minDelay = new Setting<>("RandomDelayMin", 2, 0, 20);
+    public final Setting<Integer> maxDelay = new Setting<>("RandomDelayMax", 13, 0, 20);
+
     private int delay;
     private final Random random = new Random(); // For random delay
 
     public TriggerBot() {
         super("TriggerBot", Category.COMBAT);
-        this.onlySpace = (new Setting<>("OnlyCrit", false)).addToGroup(this.smartCrit);
-        this.autoJump = (new Setting<>("AutoJump", false)).addToGroup(this.smartCrit);
-        this.ignoreWalls = new Setting<>("IgnoreWalls", false);
-        this.pauseEating = new Setting<>("PauseWhileEating", false);
-        this.requireWeapon = new Setting<>("RequireWeapon", true);
     }
 
     @EventHandler
     public void onAttack(PlayerUpdateEvent e) {
-        if (mc.field_1755 == null) {
-            if (!mc.field_1724.method_6115() || !pauseEating.getValue()) {
-                if (!mc.field_1690.field_1903.method_1434() && mc.field_1724.method_24828() && autoJump.getValue()) {
-                    mc.field_1724.method_6043();
-                }
+        if (mc.player.isUsingItem() && pauseEating.getValue()) {
+            return;
+        }
+        if (!mc.options.jumpKey.isPressed() && mc.player.isOnGround() && autoJump.getValue())
+            mc.player.jump();
 
-                if (delay > 0) {
-                    --delay;
-                } else if (autoCrit()) {
-                    if (!requireWeapon.getValue() || isHoldingWeapon()) {
-                        class_1297 ent = Managers.PLAYER.getRtxTarget(mc.field_1724.method_36454(), mc.field_1724.method_36455(), attackRange.getValue(), ignoreWalls.getValue());
-                        if (ent != null && !Managers.FRIEND.isFriend(ent.method_5477().getString())) {
-                            mc.field_1761.method_2918(mc.field_1724, ent);
-                            mc.field_1724.method_6104(class_1268.field_5808);
-
-                            // Set delay for the next hit (50 to 100 ms)
-                            delay = random.nextInt(1, 3); // Random delay of 1-2 ticks (50-100 ms)
-                        }
-                    }
-                }
+        // Smart crits should not be delayed
+        if (!autoCrit()) {
+            if (delay > 0) {
+                delay--;
+                return;
             }
+        }
+
+        Entity ent = Managers.PLAYER.getRtxTarget(mc.player.getYaw(), mc.player.getPitch(), attackRange.getValue(), ignoreWalls.getValue());
+        if (ent != null && !Managers.FRIEND.isFriend(ent.getName().getString())) {
+            mc.interactionManager.attackEntity(mc.player, ent);
+            mc.player.swingHand(Hand.MAIN_HAND);
+
+            // Set delay for the next hit (10 to 20 ms)
+            delay = random.nextInt(minDelay.getValue(), maxDelay.getValue() + 1) ; // (20ms / 50ms per tick = ~0.4 ticks, 10ms / 50ms = ~0.2 ticks)
+            // ulybaka1337: am i cooking???
+            // default delay is calculated with
+            // nextInt(11) + 2
+            // so max value is 11+2=13 and min is 2
         }
     }
 
     private boolean autoCrit() {
-        boolean reasonForSkipCrit = !smartCrit.getValue().isEnabled()
-                || mc.field_1724.method_31549().field_7479
-                || mc.field_1724.method_6128()
-                || ModuleManager.elytraPlus.isEnabled()
-                || mc.field_1724.method_6059(class_1294.field_5919)
-                || mc.field_1724.method_21754()
-                || mc.field_1687.method_8320(class_2338.method_49638(mc.field_1724.method_19538())).method_26204() == class_2246.field_10343;
+        boolean reasonForSkipCrit =
+                !smartCrit.getValue().isEnabled()
+                        || mc.player.getAbilities().flying
+                        || (mc.player.isFallFlying() || ModuleManager.elytraPlus.isEnabled())
+                        || mc.player.hasStatusEffect(StatusEffects.BLINDNESS)
+                        || mc.player.isHoldingOntoLadder()
+                        || mc.world.getBlockState(BlockPos.ofFloored(mc.player.getPos())).getBlock() == Blocks.COBWEB;
 
-        if (mc.field_1724.field_6017 > 1.0F && (double)mc.field_1724.field_6017 < 1.14) {
+        if (mc.player.fallDistance > 1 && mc.player.fallDistance < 1.14)
             return false;
-        } else if (ModuleManager.aura.getAttackCooldown() < (mc.field_1724.method_24828() ? 1.0F : 0.9F)) {
-            return false;
-        } else {
-            boolean mergeWithTargetStrafe = !ModuleManager.targetStrafe.isEnabled() || !ModuleManager.targetStrafe.jump.getValue();
-            boolean mergeWithSpeed = !ModuleManager.speed.isEnabled() || mc.field_1724.method_24828();
-            if (!mc.field_1690.field_1903.method_1434() && mergeWithTargetStrafe && mergeWithSpeed && !onlySpace.getValue() && !autoJump.getValue()) {
-                return true;
-            } else if (mc.field_1724.method_5771()) {
-                return true;
-            } else if (!mc.field_1690.field_1903.method_1434() && ModuleManager.aura.isAboveWater()) {
-                return true;
-            } else if (reasonForSkipCrit) {
-                return true;
-            } else {
-                return !mc.field_1724.method_24828() && mc.field_1724.field_6017 > 0.0F;
-            }
-        }
-    }
 
-    private boolean isHoldingWeapon() {
-        class_1792 heldItem = mc.field_1724.method_6047().method_7909();
-        return heldItem instanceof class_1829 || heldItem instanceof class_1743 || heldItem instanceof class_1810 || heldItem instanceof class_1794 || heldItem instanceof class_1835 || heldItem instanceof class_1753;
+        if (ModuleManager.aura.getAttackCooldown() < (mc.player.isOnGround() ? 1f : 0.9f))
+            return false;
+
+        boolean mergeWithTargetStrafe = !ModuleManager.targetStrafe.isEnabled() || !ModuleManager.targetStrafe.jump.getValue();
+        boolean mergeWithSpeed = !ModuleManager.speed.isEnabled() || mc.player.isOnGround();
+
+        if (!mc.options.jumpKey.isPressed() && mergeWithTargetStrafe && mergeWithSpeed && !onlySpace.getValue() && !autoJump.getValue())
+            return true;
+
+        if (mc.player.isInLava())
+            return true;
+
+        if (!mc.options.jumpKey.isPressed() && ModuleManager.aura.isAboveWater())
+            return true;
+
+        if (!reasonForSkipCrit)
+            return !mc.player.isOnGround() && mc.player.fallDistance > 0.0f;
+        return true;
     }
 }
